5cdc4606b0baf0319e2b007716d61e72ff45af62
Fabio Santos <f4bio.sa@gmail.com>
2025-08-14 16:58:41 -0400

    dev custom search button

--- classes/output/core/course_renderer.php
+++ classes/output/core/course_renderer.php
@@ -3,6 +3,13 @@
 
 namespace theme_govbrds\output\core;
 
+use html_writer;
+use coursecat_helper;
+use stdClass;
+use core_course_list_element;
+use theme_govbrds\util\course;
+use moodle_url;
+
 defined('MOODLE_INTERNAL') || die();
 
 /**
@@ -57,18 +64,10 @@ class course_renderer extends \core_course_renderer {
                 $buttonattributes = array(
                     'class' => 'br-button primary mr-3 mb-3',
                     'data-categoryid' => $category->id,
-                    'title' => $categoryname . ($coursecount > 0 ? " ({$coursecount} cursos)" : ''),
+                    'title' => $categoryname . ($coursecount > 0 ? " ( {$coursecount} )" : ''),
                     'role' => 'button'
                 );
-                
                 $buttoncontent = \html_writer::div($categoryname, 'category-name');
-                if ($coursecount > 0) {
-                    $coursestext = $coursecount == 1 ? 'curso' : 'cursos';
-                    $buttoncontent .= \html_writer::div($coursecount . ' ' . $coursestext, 'category-count');
-                } else {
-                    $buttoncontent .= \html_writer::div('Sem cursos', 'category-count category-empty');
-                }
-                
                 $output .= \html_writer::link($categoryurl, $buttoncontent, $buttonattributes);
             }
             
@@ -87,6 +86,223 @@ class course_renderer extends \core_course_renderer {
             );
             return $output;
         }
+    }
+
+
+    /**
+     * Renders the list of courses
+     *
+     * This is internal function, please use core_course_renderer::courses_list() or another public
+     * method from outside of the class
+     *
+     * If list of courses is specified in $courses; the argument $chelper is only used
+     * to retrieve display options and attributes, only methods get_show_courses(),
+     * get_courses_display_option() and get_and_erase_attributes() are called.
+     *
+     * @param coursecat_helper $chelper various display options
+     * @param array $courses the list of courses to display
+     * @param int|null $totalcount total number of courses (affects display mode if it is AUTO or pagination if applicable),
+     *     defaulted to count($courses)
+     * @return string
+     */
+    protected function coursecat_courses(coursecat_helper $chelper, $courses, $totalcount = null) {
+        global $CFG;
+        if ($totalcount === null) {
+            $totalcount = count($courses);
+        }
+        if (!$totalcount) {
+            // Courses count is cached during courses retrieval.
+            return '';
+        }
+
+        if ($chelper->get_show_courses() == self::COURSECAT_SHOW_COURSES_AUTO) {
+            // In 'auto' course display mode we analyse if number of courses is more or less than $CFG->courseswithsummarieslimit.
+            if ($totalcount <= $CFG->courseswithsummarieslimit) {
+                $chelper->set_show_courses(self::COURSECAT_SHOW_COURSES_EXPANDED);
+            } else {
+                $chelper->set_show_courses(self::COURSECAT_SHOW_COURSES_COLLAPSED);
+            }
+        }
+
+        // Prepare content of paging bar if it is needed.
+        $paginationurl = $chelper->get_courses_display_option('paginationurl');
+        $paginationallowall = $chelper->get_courses_display_option('paginationallowall');
+        if ($totalcount > count($courses)) {
+            // There are more results that can fit on one page.
+            if ($paginationurl) {
+                // The option paginationurl was specified, display pagingbar.
+                $perpage = $chelper->get_courses_display_option('limit', $CFG->coursesperpage);
+                $page = $chelper->get_courses_display_option('offset') / $perpage;
+                $pagingbar = $this->paging_bar($totalcount, $page, $perpage,
+                    $paginationurl->out(false, ['perpage' => $perpage]));
+                if ($paginationallowall) {
+                    $pagingbar .= html_writer::tag('div', html_writer::link($paginationurl->out(false, ['perpage' => 'all']),
+                        get_string('showall', '', $totalcount)), ['class' => 'paging paging-showall']);
+                }
+            } else if ($viewmoreurl = $chelper->get_courses_display_option('viewmoreurl')) {
+                // The option for 'View more' link was specified, display more link.
+                $viewmoretext = $chelper->get_courses_display_option('viewmoretext', new \lang_string('viewmore'));
+                $morelink = html_writer::tag(
+                    'div',
+                    html_writer::link($viewmoreurl, $viewmoretext, ['class' => 'btn btn-secondary']),
+                    ['class' => 'paging paging-morelink']
+                );
+            }
+        } else if (($totalcount > $CFG->coursesperpage) && $paginationurl && $paginationallowall) {
+            // There are more than one page of results and we are in 'view all' mode, suggest to go back to paginated view mode.
+            $pagingbar = html_writer::tag('div',
+                html_writer::link($paginationurl->out(false, ['perpage' => $CFG->coursesperpage]),
+                get_string('showperpage', '', $CFG->coursesperpage)), ['class' => 'paging paging-showperpage']);
+        }
+
+        // Display list of courses.
+        $attributes = $chelper->get_and_erase_attributes('courses');
+        $content = html_writer::start_tag('div', $attributes);
+
+        if (!empty($pagingbar)) {
+            $content .= $pagingbar;
+        }
+
+        $coursecount = 1;
+        $content .= html_writer::start_tag('div', ['class' => 'card-deck dashboard-card-deck mt-2']);
+        foreach ($courses as $course) {
+            $content .= $this->coursecat_coursebox($chelper, $course);
+
+            if ($coursecount % 3 == 0) {
+                $content .= html_writer::end_tag('div');
+                $content .= html_writer::start_tag('div', ['class' => 'card-deck dashboard-card-deck mt-2']);
+            }
+
+            $coursecount ++;
+        }
+
+        $content .= html_writer::end_tag('div');
+
+        if (!empty($pagingbar)) {
+            $content .= $pagingbar;
+        }
+
+        if (!empty($morelink)) {
+            $content .= $morelink;
+        }
+
+        $content .= html_writer::end_tag('div'); // End courses.
+
+        return $content;
+    }
+
+    /**
+     * Displays one course in the list of courses.
+     *
+     * This is an internal function, to display an information about just one course
+     * please use core_course_renderer::course_info_box()
+     *
+     * @param coursecat_helper $chelper various display options
+     * @param core_course_list_element|stdClass $course
+     * @param string $additionalclasses additional classes to add to the main <div> tag (usually
+     *    depend on the course position in list - first/last/even/odd)
+     * @return string
+     *
+     * @throws \coding_exception
+     * @throws \dml_exception
+     * @throws \moodle_exception
+     */
+    protected function coursecat_coursebox(coursecat_helper $chelper, $course, $additionalclasses = '') {
+        if (!isset($this->strings->summary)) {
+            $this->strings->summary = get_string('summary');
+        }
+
+        if ($chelper->get_show_courses() <= self::COURSECAT_SHOW_COURSES_COUNT) {
+            return '';
+        }
+
+        if ($course instanceof stdClass) {
+            $course = new core_course_list_element($course);
+        }
+
+        return $this->coursecat_coursebox_content($chelper, $course);
+    }
+
+    /**
+     * Returns HTML to display course content (summary, course contacts and optionally category name)
+     *
+     * This method is called from coursecat_coursebox() and may be re-used in AJAX
+     *
+     * @param coursecat_helper $chelper various display options
+     * @param stdClass|core_course_list_element $course
+     *
+     * @return string
+     *
+     * @throws \coding_exception
+     * @throws \dml_exception
+     * @throws \moodle_exception
+     */
+    protected function coursecat_coursebox_content(coursecat_helper $chelper, $course) {
+        if ($course instanceof stdClass) {
+            $course = new core_course_list_element($course);
+        }
+
+        $courseutil = new course($course);
+
+        $coursecontacts = $courseutil->get_course_contacts();
+
+        $courseenrolmenticons = $courseutil->get_enrolment_icons();
+        $courseenrolmenticons = !empty($courseenrolmenticons) ? $this->render_enrolment_icons($courseenrolmenticons) : false;
+
+        $courseprogress = $courseutil->get_progress();
+        $hasprogress = $courseprogress != null;
+
+        $data = [
+            'id' => $course->id,
+            'fullname' => $chelper->get_course_formatted_name($course),
+            'visible' => $course->visible,
+            'image' => $courseutil->get_summary_image(),
+            'summary' => $courseutil->get_summary($chelper),
+            'category' => $courseutil->get_category(),
+            'customfields' => $courseutil->get_custom_fields(),
+            'hasprogress' => $hasprogress,
+            'progress' => (int) $courseprogress,
+            'hasenrolmenticons' => $courseenrolmenticons != false,
+            'enrolmenticons' => $courseenrolmenticons,
+            'hascontacts' => !empty($coursecontacts),
+            'contacts' => $coursecontacts,
+            'courseurl' => $this->get_course_url($course->id),
+        ];
+
+        return $this->render_from_template('theme_govbrds/coursecard', $data);
+    }
+
+    /**
+     * Returns enrolment icons
+     *
+     * @param array $icons
+     *
+     * @return array
+     */
+    protected function render_enrolment_icons(array $icons): array {
+        $data = [];
+
+        foreach ($icons as $icon) {
+            $data[] = $this->render($icon);
+        }
+
+        return $data;
+    }
+
+    /**
+     * Returns the course URL based on some criterias.
+     *
+     * @param int $courseid
+     *
+     * @return moodle_url
+     * @throws \moodle_exception
+     */
+    private function get_course_url($courseid) {
+        if (class_exists('\local_course\output\index')) {
+            return new moodle_url('/local/course/index.php', ['id' => $courseid]);
+        }
+
+        return new moodle_url('/course/view.php', ['id' => $courseid]);
     }
 
 }
