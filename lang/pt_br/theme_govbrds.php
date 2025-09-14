a13f5d41762b79815bbb09e2d02c35c0d7e482b6
Fabio Santos <f4bio.sa@gmail.com>
2025-09-14 07:38:19 -0400

    Translate to pt_br

---
+++ lang/pt_br/theme_govbrds.php
@@ -0,0 +1,215 @@
+<?php
+// This file is part of Moodle - http://moodle.org/
+//
+// Moodle is free software: you can redistribute it and/or modify
+// it under the terms of the GNU General Public License as published by
+// the Free Software Foundation, either version 3 of the License, or
+// (at your option) any later version.
+//
+// Moodle is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+// GNU General Public License for more details.
+//
+// You should have received a copy of the GNU General Public License
+// along with Moodle.  If not, see <http://www.gnu.org/licenses/>.
+
+/**
+ * GovBR-DS Language file.
+ *
+ * @package    theme_govbrds
+ * @copyright  2018 Fábio Santos <fabio.santos@ifrr.edu.br>
+ * @license    http://www.gnu.org/copyleft/gpl.html GNU GPL v3 or later
+ */
+
+defined('MOODLE_INTERNAL') || die();
+
+$string['pluginname'] = 'GovBR-DS';
+$string['configtitle'] = 'GovBR-DS';
+
+$string['configtitle'] = 'GovBR-DS';
+$string['choosereadme'] = 'O tema GovBR-DS é um tema filho do Boost. Ele é baseado na identidade digital do governo brasileiro.';
+$string['configtitle'] = 'Configurações do GovBR-DS';
+
+// Aba de configurações gerais
+$string['generalsettings'] = 'Configurações Gerais';
+
+$string['organization'] = 'Organização';
+$string['organization_desc'] = 'Nome da instituição.';
+
+$string['subordination'] = 'Subordinação';
+$string['subordination_desc'] = 'Ministério ao qual pertence.';
+
+$string['layout'] = 'Layout';
+$string['layout_desc'] = 'Escolha o layout para o site.';
+
+$string['logo'] = 'Seu Logotipo';
+$string['logo_desc'] = 'Envie a imagem do logotipo que será exibida no cabeçalho do site. O tamanho recomendado é 280x80 pixels.';
+
+$string['partners'] = 'Logotipo dos Parceiros';
+$string['partners_desc'] = 'Envie a imagem do logotipo que será exibida no rodapé do site. O tamanho recomendado é 280x80 pixels.';
+
+$string['organization'] = 'Organização';
+$string['organization_desc'] = 'Descrição da instituição.';
+
+$string['organization_url'] = 'URL da Organização';
+$string['organization_url_desc'] = 'Link para a página inicial da instituição.';
+
+$string['address'] = 'Endereço';
+$string['address_desc'] = 'JSON do endereço que você deseja exibir no rodapé.';
+
+$string['addressm'] = 'Rodapé Manual';
+$string['addressm_desc'] = 'Digite algo que deseja exibir no rodapé.';
+
+$string['boostset'] = 'Configuração do Tema';
+
+$string['message_title'] = 'Título da Mensagem em Destaque';
+$string['message_title_desc'] = 'Título da mensagem em destaque na página inicial.';
+
+$string['message_content'] = 'Conteúdo da Mensagem em Destaque';
+$string['message_content_desc'] = 'Conteúdo da mensagem em destaque na página inicial.';
+
+// Aba de configurações da página inicial
+$string['homepage_settings'] = 'Configurações da Página Inicial';
+$string['homepage'] = 'Página Inicial';
+
+$string['herohtml'] = 'Seção Hero';
+$string['herohtml_desc'] = 'Conteúdo da seção hero na página inicial.';
+$string['heroimage'] = 'Imagem Hero';
+$string['heroimage_desc'] = 'Imagem a ser exibida na seção hero. Recomendada: .png (PNG-24) sem fundo, tamanho 450x600 pixels (proporção 3x4).';
+
+$string['features'] = 'Exibir Caixas de Funcionalidades';
+$string['featuresdesc'] = 'Escolha se deseja exibir a seção de funcionalidades na página inicial.';
+
+$string['featuresheading'] = 'Título da Seção de Funcionalidades';
+$string['featurescontent'] = 'Conteúdo da Seção de Funcionalidades';
+
+$string['featureheading'] = 'Título da Funcionalidade';
+$string['featurecontent'] = 'Conteúdo da Funcionalidade';
+$string['featureicon'] = 'Ícone da Funcionalidade {$a}';
+$string['featureicon_desc'] = 'Escolha um ícone para a funcionalidade {$a}. Veja a lista de ícones em https://fontawesome.com/icons?d=gallery&m=free.';
+
+$string['feature_btntext'] = 'Texto do Botão da Funcionalidade {$a}';
+$string['feature_btntext_desc'] = 'Texto do botão da funcionalidade {$a}.';
+$string['feature_btnurl'] = 'URL do Botão da Funcionalidade {$a}';
+$string['feature_btnurl_desc'] = 'URL do botão da funcionalidade {$a}.';
+
+$string['feature1heading'] = 'Título da Funcionalidade 1';
+$string['feature1content'] = 'Conteúdo da Funcionalidade 1';
+
+$string['feature1icon'] = 'Ícone da Funcionalidade {$a}';
+$string['feature2con_desc'] = 'Escolha um ícone para a funcionalidade {$a}. Veja a lista de ícones em https://fontawesome.com/icons?d=gallery&m=free.';
+
+$string['feature2_btntext'] = 'Texto do Botão da Funcionalidade {$a}';
+$string['feature2_btnurl'] = 'URL do Botão da Funcionalidade {$a}';
+
+$string['feature2heading'] = 'Título da Funcionalidade 2';
+$string['feature2content'] = 'Conteúdo da Funcionalidade 2';
+$string['feature2icon'] = 'Ícone da Funcionalidade {$a}';
+$string['feature2icon_desc'] = 'Escolha um ícone para a funcionalidade {$a}. Veja a lista de ícones em https://fontawesome.com/icons?d=gallery&m=free.';
+
+$string['feature3_btntext'] = 'Texto do Botão da Funcionalidade {$a}';
+$string['feature3_btnurl'] = 'URL do Botão da Funcionalidade {$a}';
+
+$string['feature3heading'] = 'Título da Funcionalidade 3';
+$string['feature3content'] = 'Conteúdo da Funcionalidade 3';
+$string['feature3icon'] = 'Ícone da Funcionalidade {$a}';
+$string['feature3icon_desc'] = 'Escolha um ícone para a funcionalidade {$a}. Veja a lista de ícones em https://fontawesome.com/icons?d=gallery&m=free.';
+
+$string['feature4heading'] = 'Título da Funcionalidade 4';
+$string['feature4content'] = 'Conteúdo da Funcionalidade 4';
+$string['feature4icon'] = 'Ícone da Funcionalidade {$a}';
+$string['feature4icon_desc'] = 'Escolha um ícone para a funcionalidade {$a}. Veja a lista de ícones em https://fontawesome.com/icons?d=gallery&m=free.';
+
+$string['feature4_btntext'] = 'Texto do Botão da Funcionalidade {$a}';
+$string['feature4_btnurl'] = 'URL do Botão da Funcionalidade {$a}';
+
+// Template da barra de navegação
+$string['courses'] = 'Cursos';
+
+// Template de landing page
+$string['searchtext'] = 'Texto de busca';
+$string['about'] = 'Sobre';
+$string['how_do_you_searh'] = 'Como você busca?';
+$string['search'] = 'Buscar';
+$string['development_team'] = 'Equipe de Desenvolvimento';
+$string['course_details'] = 'Detalhes do Curso';
+$string['related_courses'] = 'Cursos Relacionados';
+$string['course_deadline'] = 'Prazo do Curso';
+$string['startdate'] = 'Data de Início';
+$string['enddate'] = 'Data de Término';
+$string['no_related_course'] = 'Nenhum curso relacionado';
+$string['no_teacher'] = 'Sem professor';
+
+// Template de rodapé
+$string['partners'] = 'Parceiros';
+$string['useful_links'] = 'Links Úteis';
+$string['contact'] = 'Contato';
+$string['address'] = 'Endereço';
+$string['phone'] = 'Telefone';
+$string['email'] = 'E-mail';
+$string['all_rights_reserved'] = 'Todos os direitos reservados';
+$string['privacy_policy'] = 'Política de Privacidade';
+$string['terms_of_use'] = 'Termos de Uso';
+$string['accessibility'] = 'Acessibilidade';
+$string['sitemap'] = 'Mapa do Site';
+$string['follow_us'] = 'Siga-nos';
+$string['to'] = 'até';
+
+// Template de funcionalidades
+$string['how_to'] = 'Como funciona?';
+
+// Template de cabeçalho
+$string['brandcolor'] = 'Cor da Marca';
+$string['brandcolor_desc'] = 'Selecione uma cor para aplicar a links e títulos.';
+
+// Aba de configurações sociais
+$string['socialsettings'] = 'Configurações Sociais';
+$string['tiktok'] = 'Tik Tok';
+$string['tiktokdesc'] = 'Link para seu perfil no Tik Tok.';
+$string['facebook'] = 'Facebook';
+$string['facebookdesc'] = 'Link para seu perfil no Facebook.';
+$string['twitter'] = 'Twitter';
+$string['twitterdesc'] = 'Link para seu perfil no Twitter.';
+$string['linkedin'] = 'LinkedIn';
+$string['linkedindesc'] = 'Link para seu perfil no LinkedIn.';
+$string['youtube'] = 'YouTube';
+$string['youtubedesc'] = 'Link para seu perfil no YouTube.';
+$string['instagram'] = 'Instagram';
+$string['instagramdesc'] = 'Link para seu perfil no Instagram.';
+
+// BOOST
+$string['advancedsettings'] = 'Configurações Avançadas';
+$string['backgroundimage'] = 'Imagem de Fundo';
+$string['backgroundimage_desc'] = 'Imagem a ser exibida como fundo do site. A imagem enviada substituirá a imagem padrão nos arquivos do tema.';
+
+$string['brandcolor'] = 'Cor da Marca';
+$string['brandcolor_desc'] = 'Cor de destaque.';
+
+$string['currentinparentheses'] = '(atual)';
+
+$string['presetfiles'] = 'Arquivos de Predefinição Adicionais do Tema';
+$string['presetfiles_desc'] = 'Arquivos de predefinição podem ser usados para alterar drasticamente a aparência do tema. Veja <a href=https://docs.moodle.org/dev/Boost_Presets>Boost presets</a> para informações sobre como criar e compartilhar suas próprias predefinições, e confira o <a href=http://moodle.net/boost>Repositório de Predefinições</a> para ver predefinições compartilhadas por outros.';
+
+$string['preset'] = 'Predefinição do Tema';
+$string['preset_desc'] = 'Escolha uma predefinição para alterar a aparência do tema.';
+
+$string['rawscss'] = 'SCSS Bruto';
+$string['rawscss_desc'] = 'Use este campo para fornecer código SCSS ou CSS que será injetado ao final da folha de estilos.';
+
+$string['rawscsspre'] = 'SCSS Inicial Bruto';
+$string['rawscsspre_desc'] = 'Use este campo para fornecer código SCSS de inicialização. Ele será injetado antes de tudo. Na maioria das vezes, você usará esta configuração para definir variáveis.';
+
+/* Regiões */
+$string['region-side-post'] = 'Direita';
+$string['region-side-pre'] = 'Esquerda';
+$string['region-home-left'] = 'Início (Esquerda)';
+$string['region-home-middle'] = 'Início (Centro)';
+$string['region-home-right'] = 'Início (Direita)';
+$string['region-footer-left'] = 'Rodapé (Esquerda)';
+$string['region-footer-middle'] = 'Rodapé (Centro)';
+$string['region-footer-right'] = 'Rodapé (Direita)';
+$string['region-hidden-dock'] = 'Oculto dos usuários';
+
+$string['privacy:metadata:govbrds_user_setting'] = 'Armazena preferências de configuração do tema específicas do usuário.';
+
