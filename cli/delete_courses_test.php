eb09bc4af9d577f5b15a2ced6524e10cce908c30
f4biosa <f44bio.sa@gmail.com>
2025-11-25 18:08:51 -0400

    Checks code against some aspects of the Moodle coding guidelines.

--- cli/delete_courses_test.php
+++ cli/delete_courses_test.php
@@ -1,11 +1,26 @@
 <?php
+// This file is part of Moodle - http://moodle.org/
+//
+// Moodle is free software: you can redistribute it and/or modify
+// it under the terms of the GNU General Public License as published by
+// the Free Software Foundation, either version 3 of the License, or
+// (at your option) any later version.
+//
+// Moodle is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+// GNU General Public License for more details.
+//
+// You should have received a copy of the GNU General Public License
+// along with Moodle.  If not, see <http://www.gnu.org/licenses/>.
+
 /**
- * Script CLI para deletar cursos de teste no Moodle
+ * CLI script to delete trial courses in Moodle.
  * 
- * Este script deleta cursos criados pelo script create_test_courses.php
+ * This script deletes courses created by the create_test_courses.php script.
  * 
- * Uso:
- * php delete_test_courses.php --prefix="Curso Teste"
+ * Use:
+ * php delete_courses_test.php --prefix="Course Test"
  * 
  * @package    theme_govbrds
  * @copyright  2025
