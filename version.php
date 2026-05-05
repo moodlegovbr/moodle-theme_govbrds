526b65f258857f4bddf3e01c487a99b201379d33
f4biosa <f4bio.sa@gmail.com>
2026-05-05 11:13:40 -0400

    fix tab itens in onetopic courses

--- version.php
+++ version.php
@@ -30,7 +30,7 @@ defined("MOODLE_INTERNAL") || die();
 $plugin->component = "theme_govbrds";
 
 // This is the version of the plugin.
-$plugin->version = 2026021602;
+$plugin->version = 2026050500;
 
 // This is the named version.
 $plugin->release = "v2.21 alpha";
